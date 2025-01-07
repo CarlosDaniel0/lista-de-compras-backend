e1feb7c0461dca7c71f05a31b59b9a4f6b720c60
Carlos Daniel <carlos.daniel2016@hotmail.com>
2025-01-07 00:49:30 -0300

    Feature: remove time limit of user login

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,7 +3,6 @@ import adapter from "../../database/prisma";
 import { PrismaClient } from "@prisma/client";
 import { databaseErrorResponse } from "../../utils/constants";
 import { CredentialResponse } from "../../entities/CredentialSignIn";
-import { jwtDecode } from "jwt-decode";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -11,34 +10,25 @@ router.post("/", async (req, res) => {
 
   try {
     const credential = CredentialResponse.parse(req.body);
-    const token = jwtDecode(credential.credential);
-    if ((token?.exp ?? 0) * 1000 < Date.now())
-      res
-        .status(401)
-        .send({
-          status: false,
-          message: "Usuário não autenticado, faça login novamente",
-        });
-    else {
-      let user = await prisma.user.findFirst({
-        where: {
-          email: credential.user?.email,
-          name: credential.user?.name,
-        },
-      });
-      if (!user && credential.user) {
-        user = await prisma.user.create({
-          data: credential.user.toEntity(),
-        });
-      }
 
-      res.send({
-        status: true,
-        data: {
-          user: Object.assign(user ?? {}, { token: credential.credential }),
-        },
+    let user = await prisma.user.findFirst({
+      where: {
+        email: credential.user?.email,
+        name: credential.user?.name,
+      },
+    });
+    if (!user && credential.user) {
+      user = await prisma.user.create({
+        data: credential.user.toEntity(),
       });
     }
+
+    res.send({
+      status: true,
+      data: {
+        user: Object.assign(user ?? {}, { token: credential.credential }),
+      },
+    });
   } catch (e: any) {
     res.send(databaseErrorResponse(e?.message ?? ""));
   }
