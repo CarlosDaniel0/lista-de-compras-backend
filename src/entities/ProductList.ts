2b1ac539c2d5e0003b29683d7f73739c4773cf12
Carlos Daniel <carlos.daniel2016@hotmail.com>
2025-01-22 22:42:51 -0300

    Bugfix: Adding new fields of ProductList table

--- src/entities/ProductList.ts
+++ src/entities/ProductList.ts
@@ -1,5 +1,6 @@
 import { List } from "./List";
 import { ProductSupermarket } from "./ProductSupermarket";
+import { Supermarket } from "./Supermarket";
 
 export class ProductList {
   id: string;
@@ -8,8 +9,10 @@ export class ProductList {
   quantity: number;
   list_id: string;
   product_id?: string;
+  supermarket_id?: string;
   list?: List;
   product?: ProductSupermarket;
+  supermarket?: Supermarket;
 
   constructor(
     id: string,
@@ -18,8 +21,10 @@ export class ProductList {
     quantity: number,
     list_id: string,
     product_id?: string,
+    supermarket_id?: string,
     list?: Record<string, any>,
-    product?: Record<string, any>
+    product?: Record<string, any>,
+    supermarket?: Record<string, any>
   ) {
     this.id = id;
     this.description = description;
@@ -27,18 +32,20 @@ export class ProductList {
     this.quantity = quantity;
     this.list_id = list_id;
     if (product_id) this.product_id = product_id;
+    if (supermarket_id) this.supermarket_id = supermarket_id;
     if (list) this.list = List.parse(list);
     if (product) this.product = ProductSupermarket.parse(product);
+    if (supermarket) this.supermarket = Supermarket.parse(supermarket)
   }
 
   static parse(json: Record<string, any>) {
-    const { id, description, unity, quantity, list_id, product_id, list, product } = json;
+    const { id, description, unity, quantity, list_id, supermarket_id, product_id, list, product, supermarket } = json;
     if (!description) throw new Error("O campo description é obrigatório");
     if (!unity) throw new Error("O campo unity é obrigatório");
     if (typeof quantity !== "number" && Number.isNaN(Number(quantity)))
       throw new Error("O campo quantity é obrigatório");
     if (!list_id) throw new Error("O campo list_id é obrigatório");
-    return new ProductList(id, description, unity, quantity, list_id, product_id, list, product);
+    return new ProductList(id, description, unity, quantity, list_id, supermarket_id, product_id, list, product, supermarket);
   }
 
   toEntity() {
@@ -48,7 +55,8 @@ export class ProductList {
       unity: this.unity,
       quantity: this.quantity,
       list_id: this.list_id,
-      product_id: this.product_id
+      product_id: this.product_id,
+      supermarket_id: this.supermarket_id
     };
 
     return json;
