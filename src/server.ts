d2d19feada78e1d85d9688aef22d17679aae4a77
Carlos Daniel <carlos.daniel2016@hotmail.com>
2025-02-04 14:52:40 -0300

    BugFix: Enhance body size for accept XML in content of request

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
 app.use(cors())
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
 app.use(router)
